3adba6eaa3b8b4a2dd11f7304e0d831b297641d5
Similoluwa Abidoye <154632691+Abidoyesimze@users.noreply.github.com>
2024-04-19 22:48:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Videos from './Videos';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <div className="main-display" style={{"display" : "flex"}}>
-      <Sidebar/>
-       <Videos/>
-      </div>
-    </div>
-  );
-}
 
-export default App;
+import { Navbar, SearchFeed, VideoDetail, ChannelDetail, Tube, Feed } from './components';
+import './index.css';
+
+
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000', padding: 1 }}>
+            <Navbar />
+            <Feed />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetail />} />
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
+
+export default App
